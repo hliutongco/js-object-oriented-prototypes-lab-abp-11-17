00884d99b7d346f9f8b1af376ef901deeea3a430
Helen Liutongco <bellpickle@gmail.com>
2017-11-12 02:23:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ BoardMember.prototype.approve = function () {
   return "You can do that!"
 }
 
+BoardMember.prototype.doCharity = function () {
+  return "I like to help people."
+}
+
 //
 //   this.veto = function(){
 //     return 'No, I must disagree'
